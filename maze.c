1743378392390caa93c55a2699efadd631ed3999
Tadeas Horak <horaktada@seznam.cz>
2023-11-19 02:33:00 +0100

    working argument parser kinda

--- maze.c
+++ maze.c
@@ -12,25 +12,25 @@ int ParseArgs(char **arguments, int argumentCount) {
     }
     else if (strcmp("--test", arguments[i]) == 0){
         return 1;
-    }/*
-    else if(strcmp("-n", arguments[i]) == 0){
-      if(++i>=argumentCount)
-        return 0;
-      if(config->nValue!=10)
-        return 0;
-      config->nValue=atoi(arguments[i]);
     }
-    else if(strcmp("-s", arguments[i]) == 0){
-      if(++i>=argumentCount)
+    else if(strcmp("--lpath", arguments[i]) == 0){
+      if(i+2>=argumentCount)
         return 0;
-      if(config->sValue!=(NULL))
+      int R = atoi(arguments[i+1]);
+      int C = atoi(arguments[i+2]);
+      return R+C;
+    }
+    else if(strcmp("--rpath", arguments[i]) == 0){
+      if(i+2>=argumentCount)
         return 0;
-      config->sValue=arguments[i];
-    }*/
+      int R = atoi(arguments[i+1]);
+      int C = atoi(arguments[i+2]);
+      return R+C+1;
+    }
   }
   return 2;
 }
 int main(int argc, char **argv){
-    Printf("%d", ParseArgs(argv, argc))
+    printf("%d", ParseArgs(argv, argc));
     return 0;
 }
