b99be2a68d9e8861ec388e5a9b847a15998354d0
Tadeas Horak <horaktada@seznam.cz>
2023-11-19 02:08:16 +0100

    arguments parser added ig

--- maze.c
+++ maze.c
@@ -2,7 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char **argv){
 
+int ParseArgs(char **arguments, int argumentCount) {
+  if(argumentCount<0)
+    return 0;
+  for(int i=1; i<argumentCount; i++){
+    if(strcmp("--help", arguments[i]) == 0){
+      return 0;
+    }
+    else if (strcmp("--test", arguments[i]) == 0){
+        return 1;
+    }/*
+    else if(strcmp("-n", arguments[i]) == 0){
+      if(++i>=argumentCount)
+        return 0;
+      if(config->nValue!=10)
+        return 0;
+      config->nValue=atoi(arguments[i]);
+    }
+    else if(strcmp("-s", arguments[i]) == 0){
+      if(++i>=argumentCount)
+        return 0;
+      if(config->sValue!=(NULL))
+        return 0;
+      config->sValue=arguments[i];
+    }*/
+  }
+  return 2;
+}
+int main(int argc, char **argv){
+    Printf("%d", ParseArgs(argv, argc))
     return 0;
 }
