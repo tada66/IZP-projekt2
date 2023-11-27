8193c3f480e9379635c2b283edd2060e06c80dac
Tadeas Horak <horaktada@seznam.cz>
2023-11-27 02:06:07 +0100

    better error handling

--- maze.c
+++ maze.c
@@ -12,74 +12,71 @@ int main(int argc, char **argv){
 int ParseArgs(char **arguments, int argumentCount) {
   if(argumentCount<0)
     return 0;
-  for(int i=1; i<argumentCount; i++){
-    if(strcmp("--help", arguments[i]) == 0){
-      if(argumentCount>2){
-        fprintf(stderr, "Too many arguments!\n");
-        return -1;
-      }
-      PrintHelp();
+  int i=1;
+  if(strcmp("--help", arguments[i]) == 0){
+    if(argumentCount>2){
+      fprintf(stderr, "Too many arguments!\n");
+      return -1;
+    }
+    PrintHelp();
+    return 0;
+  }
+  else if (strcmp("--test", arguments[i]) == 0){
+    if(++i>=argumentCount){
+      fprintf(stderr, "Not enough arguments\n");
       return 0;
     }
-    else if (strcmp("--test", arguments[i]) == 0){
-      if(++i>=argumentCount){
-        fprintf(stderr, "Not enough arguments\n");
-        return 0;
-      }
-      Map map;
-      if(!MapInit(&map, arguments[i]))
-        return -1;
-      if(MapTest(&map))
-        printf("Valid\n");
-      else
-        printf("Invalid\n");
-      MapDtor(&map);
-    }
-    else if(strcmp("--lpath", arguments[i]) == 0){
-      if(i+2>=argumentCount)
-        return 0;
-      int R = atoi(arguments[i+1]);
-      int C = atoi(arguments[i+2]);
-      Map map;
-      if(MapInit(&map, arguments[i+3]))
-        return 1;
-      return R+C;
-    }
-    else if(strcmp("--rpath", arguments[i]) == 0){
-      if(i+2>=argumentCount)
-        return 0;
-      int R = atoi(arguments[i+1]);
-      int C = atoi(arguments[i+2]);
-      return R+C+1;
-    }
-    else if(strcmp("--isb", arguments[i]) == 0){
-      if(i+4>=argumentCount)
-        return 0;
-      Map map;
-      if(!MapInit(&map, arguments[i+1]))
-        return -1;
-      int R = atoi(arguments[i+2]);
-      int C = atoi(arguments[i+3]);
-      int b = atoi(arguments[i+4]);
-      printf("is border: %d\n", isborder(&map, R, C, b));
-      return -2;
-    }
-    else if(strcmp("--stb", arguments[i]) == 0){
-      if(i+4>=argumentCount)
-        return 0;
-      Map map;
-      if(!MapInit(&map, arguments[i+1]))
-        return -1;
-      int R = atoi(arguments[i+2]);
-      int C = atoi(arguments[i+3]);
-      int lr = atoi(arguments[i+4]);
-      
-      int rotation = start_border(&map, R, C, lr);
-      if(rotation!=-1)
-        Mazefollower(&map, R, C, lr, rotation);
-      return -2;
-    }
+    Map map;
+    if(!MapInit(&map, arguments[i]))
+      return -1;
+    if(MapTest(&map))
+      printf("Valid\n");
+    else
+      printf("Invalid\n");
+    MapDtor(&map);
   }
+  else if(strcmp("--lpath", arguments[i]) == 0){
+    if(i+2>=argumentCount){
+      fprintf(stderr, "Too few arguments!\n");
+      return 0;
+    }
+    else if(5<argumentCount){
+      fprintf(stderr, "Too many arguments!\n");
+    }
+    int R = atoi(arguments[i+1]);
+    int C = atoi(arguments[i+2]);
+    Map map;
+    if(MapInit(&map, arguments[i+3]))
+      return 1;
+    return R+C;
+  }
+  else if(strcmp("--rpath", arguments[i]) == 0){
+    if(i+2>=argumentCount)
+      return 0;
+    int R = atoi(arguments[i+1]);
+    int C = atoi(arguments[i+2]);
+    return R+C+1;
+  }
+  else if(strcmp("--stb", arguments[i]) == 0){
+    if(i+4>=argumentCount)
+      return 0;
+    Map map;
+    if(!MapInit(&map, arguments[i+1]))
+      return -1;
+    int R = atoi(arguments[i+2]);
+    int C = atoi(arguments[i+3]);
+    int lr = atoi(arguments[i+4]);
+    
+    int rotation = start_border(&map, R, C, lr);
+    if(rotation!=-1)
+      Mazefollower(&map, R, C, lr, rotation);
+    return -2;
+  }
+  else{
+    fprintf(stderr, "Invalid argument!\n");
+      return -1;
+  }
+
   return -1;
 }
 
@@ -89,9 +86,6 @@ void PrintHelp(){
   printf("--test will check any file for a valid maze definition, return Valid or Invalid\n");
   printf("--rpath R C needs two arguments R, and C, which will be the coordinates for the entry point into the maze, --rpath then looks for an exit using the right hand rule\n");
   printf("--lpath R C needs two arguments R, and C, which will be the coordinates for the entry point into the maze, --lpath then looks for an exit using the left hand rule\n");
-  printf("----------DEBUG ONLY----------\n");
-  printf("--isb test for isborder, needs maze file, r, c, border to check - 0 for bottom-top, 1 for right, 2 for left\n");
-  printf("--stb test for start_border, needs maze file, r, c, direction to go - 0 for left, 1 for right\n");
 }
 
 bool MapTest(Map *map){                 //TODO: check if some arguments are missing
