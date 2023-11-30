161d582bdfee52b0b1efcb02367f4b8d980a72c7
Tadeas Horak <horaktada@seznam.cz>
2023-11-30 23:19:55 +0100

    types.h deprecated

--- maze.c
+++ maze.c
@@ -1,8 +1,25 @@
-#include "types.h"
 #include <stdio.h>    //printf, fprintf, getc, fopen, fclose
 #include <stdlib.h>   //atoi, malloc, free
 #include <string.h>   //strcmp
 #include <stdbool.h>  //bool  
+
+typedef struct {
+  int rows;
+  int cols;
+  unsigned char *cells;
+} Map;
+
+void PrintHelp();
+int ParseArgs(char **arguments, int argumentCount);
+bool MapTest(Map *map);
+bool MapCtor(Map *map, int cols, int rows);
+void MapDtor(Map *map);
+bool MapInit(Map *map, char* arg);
+bool isborder(Map *map, int r, int c, int border);
+int start_border(Map *map, int r, int c, int leftright);
+bool FitsInMap(Map *map, int r, int c);
+int next_rotation(Map *map, int r, int c, int leftright, int rotation);
+void Mazefollower(Map *map, int r, int c, int leftright, int rotation);
 
 int main(int argc, char **argv){
   int parse = ParseArgs(argv, argc);
